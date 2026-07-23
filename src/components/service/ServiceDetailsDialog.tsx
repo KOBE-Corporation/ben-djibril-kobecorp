9bdf66907d3f57efbcd8fda1cb4bdde6a2e6ea6d
Ben Djibril <konedjibril2004@gmail.com>
2026-07-23 12:31:30 +0100

    refactor: remove unused FaWhatsapp icon import in ServiceDetailsDialog for cleaner code

--- src/components/service/ServiceDetailsDialog.tsx
+++ src/components/service/ServiceDetailsDialog.tsx
@@ -16,7 +16,6 @@ import {
   AcademicCapIcon,
   ExclamationTriangleIcon,
 } from '@heroicons/react/24/outline'
-import { FaWhatsapp } from 'react-icons/fa6'
 import { useLocale } from '../../hooks/useLocale'
 
 export type PlanId =
